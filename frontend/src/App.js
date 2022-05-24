0a00c055a72378ac81b3f194cd25d78edbd158d7
m-triple-m <triplem656@gmail.com>
2022-05-24 12:08:12 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import SignIn from "./components/main/signIn";
 import SignUp from "./components/main/signUp";
 import ResetPassword from "./components/main/resetPassword";
@@ -13,31 +12,27 @@ import AddLocation from "./components/admin/addLocation";
 // import Header from "./components/main/header";
 import NavBar from "./components/main/navBar";
 import BrowseLocation from "./components/main/browseLocation";
-
-
+import Home from "./components/main/home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route element={<Admin />} path="admin">
           <Route element={<Dashboard />} path="dashboard" />
           <Route element={<Profile />} path="profile" />
-        <Route element={<AddLocation/>}path="addLocation"/>
+          <Route element={<AddLocation />} path="addLocation" />
         </Route>
         <Route element={<User />} path="user"></Route>
         <Route element={<Main />} path="main">
+          <Route element={<SignUp />} path="signUp" />
+          <Route element={<Home />} path="home" />
+          <Route element={<SignIn />} path="signIn" />
+          <Route element={<BrowseLocation />} path="browseLocation" />
+          <Route element={<ResetPassword />} path="resetPassword" />
         </Route>
-        <Route element={<NavBar/>} path="navBar"/>
-        <Route element={<SignUp />} path="signUp"/>
-        <Route element={<SignIn />} path="signIn"/>
-        <Route element={<BrowseLocation/>} path="browseLocation"/>
-        <Route element={<ResetPassword />} path="resetPassword"/>
-        
-       
-        
-        
+
+        <Route element={<Navigate to="/main/home" />} path="/"></Route>
       </Routes>
     </BrowserRouter>
   );
