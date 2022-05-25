9d2fa8b99a9ace5e237a3811cbbf5cbce39ba705
anubhav72 <omaranubhav.a72@gmail.com>
2022-05-25 13:51:07 +0530

    ..

---
+++ frontend/src/components/main/bookingSpace.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const bookingSpace = () => {
+  return (
+    <div><div class="card border border-primary shadow-0 ">
+    <div class="card-header">Featured</div>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk of the
+        card's content.
+      </p>
+  
+      <button type="button" class="btn btn-primary">Button</button>
+    </div>
+  </div></div>
+  )
+}
+
+export default bookingSpace
